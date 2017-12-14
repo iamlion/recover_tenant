7981fbde1db4327819a2f7b045e44ba0039f2304
zhudezhen <1748439277@qq.com>
2017-12-14 20:32:54 +0800

    1.完成了个人中心初稿。 2.添加了点素材。 3.修改了登录页。

--- app/recover_tenant_mine.js
+++ app/recover_tenant_mine.js
@@ -28,8 +28,184 @@ export default class extends React.Component {
     render(){
 
         return (
-            <View>
-                <Text>abcd</Text>
+            <View
+               style={{
+                    justifyContent:"center",
+                   alignItems:"center",
+               }}
+            >
+
+                <Image
+                    style={{
+                        width:"100%",
+                        height:350,
+                    }}
+                    source={require("./assets/icon7.png")}
+                />
+
+                <View
+                   style={{
+                       justifyContent:"center",
+                       width:"100%",
+                       height:60,
+                       textAlign:"left",
+                   }}
+
+                >
+                    <Text
+                        style={{
+                            color:"#676767",
+                            fontSize:20,
+
+                        }}
+                    >
+                        修改密码
+                    </Text>
+
+                    <Image
+                        style={{
+                            width:15,
+                            height:15,
+                            position:"absolute",
+                            right:0,
+                            Top:0,
+                        }}
+                        source={require("./assets/icon8.png")}
+                    />
+                    <View
+                        style={{
+                            height:1,
+                            backgroundColor:"#d9d9d9",
+                            width:"100%",
+                        }}
+                    >
+
+                    </View>
+
+                </View>
+
+                <View
+                    style={{
+                        justifyContent:"center",
+                        width:"100%",
+                        height:60,
+                        textAlign:"left",
+                    }}
+
+                >
+                    <Text
+                        style={{
+                            color:"#676767",
+                            fontSize:20,
+
+                        }}
+                    >
+                     我的回收
+                    </Text>
+
+                    <Image
+                        style={{
+                            width:15,
+                            height:15,
+                            position:"absolute",
+                            right:0,
+                            Top:0,
+                        }}
+                        source={require("./assets/icon8.png")}
+                    />
+
+                </View>
+                <View
+                    style={{
+                        height:1,
+                        backgroundColor:"#d9d9d9",
+                        width:"100%",
+                    }}
+                >
+
+                </View>
+
+                <View
+                    style={{
+                        justifyContent:"center",
+                        width:"100%",
+                        height:60,
+                        textAlign:"left",
+                    }}
+
+                >
+                    <Text
+                        style={{
+                            color:"#676767",
+                            fontSize:20,
+
+                        }}
+                    >
+                        退出登录
+                    </Text>
+
+                    <Image
+                        style={{
+                            width:15,
+                            height:15,
+                            position:"absolute",
+                            right:0,
+                            Top:0,
+                        }}
+                        source={require("./assets/icon8.png")}
+                    />
+
+                </View>
+
+                <View
+                    style={{
+                        height:1,
+                        backgroundColor:"#d9d9d9",
+                        width:"100%",
+                    }}
+                >
+
+                </View>
+                <View
+                    style={{
+                        height:1,
+                        backgroundColor:"#d9d9d9",
+                        width:"100%",
+                    }}
+                >
+
+                </View>
+
+                <View
+                     style={{
+                         justifyContent:"center",
+                         alignItems:"center",
+                         position:"absolute",
+                         top:100,
+                     }}
+                >
+                    <Image
+                        style={{
+                            width:150,
+                            height:150,
+                        }}
+
+                        source={require("./assets/icon6.png")}
+                    />
+                    <Text
+                        style={{
+                            marginTop:15,
+                            backgroundColor:"transparent",
+                            color:"#ffffff",
+                            fontSize:25,
+                        }}
+                    >
+                        Adrienne
+                    </Text>
+
+
+
+                </View>
             </View>
         )
     }
